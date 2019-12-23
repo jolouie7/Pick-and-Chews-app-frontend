615699d1ea310680498c2cad5acd75bd7d38e0f4
Your_Name <joseph161216@gmail.com>
2019-12-23 14:46:39 -0800

    add buttons for the filters

--- src/index.js
+++ src/index.js
@@ -11,7 +11,33 @@ const renderMainPage = `
   <div>
     <h1>Pick and Chews</h1>
     <button>Find Restaurants</button>
-    <p>Filters</p>
+    <h3>Filters</h3>
+    <div>
+      <h4>Distance</h4>
+      <button type="button">5 Miles</button>
+      <button type="button">10 Miles</button>
+      <button type="button">20 Miles</button>
+    </div>
+    <div>
+      <!-- Remove this h4 later -->
+      <h4>Only Restaurants that are open now</h4>
+      <button type="button">Open now</button>
+    </div>
+    <div>
+      <h4>Star Ratings</h4>
+      <button type="button">1 Star</button>
+      <button type="button">2 Star</button>
+      <button type="button">3 Star</button>
+      <button type="button">4 Star</button>
+      <button type="button">5 Star</button>
+    </div>
+    <div>
+      <h4>Price</h4>
+      <button type="button">1 Dollar Sign</button>
+      <button type="button">2 Dollar Sign</button>
+      <button type="button">3 Dollar Sign</button>
+      <button type="button">4 Dollar Sign</button>
+    </div>
     <p>emdbeded yelp</p>
   </div>
 `
