aa93a47bd9ad1cc906297e23097058390dc16ff4
mwong068 <mwong068@gmail.com>
2020-01-01 20:22:31 -0800

    user information is persisting to database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 /* Nav Bar */
-
-
 function myFunction(x) {
     x.classList.toggle("change");
     let navBar = document.getElementById("mySidenav");
@@ -19,20 +17,17 @@ function myFunction(x) {
     }
   }
 
-const loginForm = document.querySelector("#login");
-console.log(loginForm)
-const loginPage = document.querySelector("#login-page");
-
-const renderMainPage = `
-<div id="main">
-
+  /* main page */
+  const renderMainPage = `
+  <div id="main">
+  
     <div id="mySidenav" class="sidenav">
-    <a href="#">Homepage</a>
-    <a href="#">Last Visited</a>
-    <a href="#">Top Favorited</a>
+    <a href="#" id="homepage">Homepage</button>
+    <a href="#" id="last-visited">Last Visited</a>
+    <a href="#" id="top-favorited">Top Favorited</a>
     <a href="#" id="profile">Profile</a>
     </div>
-
+    
     <!-- Use any element to open the sidenav -->
     <div class="hamburger-icon" onclick="myFunction(this)">
     <div class="bar1"></div>
@@ -41,69 +36,95 @@ const renderMainPage = `
     </div>
     
     <div id="main-content">
-      <h1>Pick and Chews</h1>
-      <button id="findRestaurant">Find Restaurants</button><br><br><br>
-      <h3>Filters</h3>
-      <div id="filters-div">
-        <div>
-            <h4>Distance</h4>
+    <h1>Pick and Chews</h1>
+    <button id="find-restaurant">Find Restaurants</button><br><br><br>
+    <h3>Filters</h3>
+    <div id="filters-div">
+    <div>
+    <h4>Distance</h4>
             <button type="button">5 Miles</button>
             <button type="button">10 Miles</button>
             <button type="button">20 Miles</button>
+            </div>
+        <div>
+        <!-- Remove this h4 later -->
+        <h4>Only Restaurants that are open now</h4>
+        <button type="button">Open now</button>
         </div>
         <div>
-            <!-- Remove this h4 later -->
-            <h4>Only Restaurants that are open now</h4>
-            <button type="button">Open now</button>
+        <h4>Star Ratings</h4>
+        <button type="button">1 Star</button>
+        <button type="button">2 Star</button>
+        <button type="button">3 Star</button>
+        <button type="button">4 Star</button>
+        <button type="button">5 Star</button>
         </div>
         <div>
-            <h4>Star Ratings</h4>
-            <button type="button">1 Star</button>
-            <button type="button">2 Star</button>
-            <button type="button">3 Star</button>
-            <button type="button">4 Star</button>
-            <button type="button">5 Star</button>
-        </div>
-        <div>
-            <h4>Price</h4>
-            <button type="button">$</button>
-            <button type="button">$$</button>
-            <button type="button">$$$</button>
-            <button type="button">$$$$</button>
+        <h4>Price</h4>
+        <button type="button">$</button>
+        <button type="button">$$</button>
+        <button type="button">$$$</button>
+        <button type="button">$$$$</button>
         </div>
         <br>
         </div>
         </div>
-    </div>
-</div>
-`;
+        </div>
+        </div>
+        `;
+        
 
+/* Login */
+const loginForm = document.querySelector("#login");
+console.log(loginForm);
+const loginPage = document.querySelector("#login-page");
+console.log(loginPage);
 loginForm.addEventListener("submit", function(e) {
-  loginPage.style.display = "none";
-  document.querySelector("body").innerHTML = renderMainPage;
+//   loginPage.style.display = "none";
+    let nameValue = event.target.name.value;
+    // debugger
+
+    let userInfo = {
+        username: nameValue
+    }
+    
+    let submitObj = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userInfo)
+    }
+
+    fetch('http://127.0.0.1:3000/users', submitObj)
+    .then(function(response) {
+        response.json().then(function(results){
+            currentUserId = results.data.id;
+            currentUserName = results.data.attributes.username;
+        })
+    })
+
+    document.querySelector("body").innerHTML = renderMainPage;
+  findARestaurant();
   displayUserProfile();
+  displayHomepage();
+  displayLastVisited();
 })
 
-loginForm.addEventListener("submit", function(e) {
-    findARestaurant();
-    displayUserProfile();
 
-})
 
 function findARestaurant() {
-    loginPage.style.display = "none";
-    document.querySelector("body").innerHTML = renderMainPage;
-    
     const resultsPage = `
     <div id="results">
     <h1>Your Results!</h1>   
     </div>
     `;
-
-    const restaurantButton = document.querySelector("#findRestaurant");
+    
+    const restaurantButton = document.querySelector("#find-restaurant");
     restaurantButton.addEventListener('click', function(event) {
         
-    document.querySelector("body").innerHTML += resultsPage;
+        document.querySelector("#main").innerHTML += resultsPage;
 
       /* Yelp Api get Data */
       var myurl =
@@ -121,9 +142,9 @@ function findARestaurant() {
       success: function(data) {
           // Grab the results from the API JSON return
           const totalResults = data.businesses;
-
+          
           if (totalResults.length > 0) {
-
+              
             for(let i=0; i < 3; i++){
                 let randomRestaurant = totalResults[Math.floor ( Math.random() * totalResults.length)];
                 let restaurantImage = randomRestaurant.image_url;
@@ -139,15 +160,15 @@ function findARestaurant() {
                     <p>Location: ${randomRestaurant.location.address1}, ${randomRestaurant.location.address2}<br>${randomRestaurant.location.city}, ${randomRestaurant.location.zip_code}</p>
                     <p>Phone number: ${randomRestaurant.display_phone}</p>
                     `;
-            let resultsDiv = document.querySelector("#results");
-            resultsDiv.appendChild(resultRestaurant);
-            };
+                    let resultsDiv = document.querySelector("#results");
+                    resultsDiv.appendChild(resultRestaurant);
+                };
           
           } else {
-          //   let resultsDiv = document.querySelector("#results");
-          $("#results").append("<h5>We discovered no results! Edit your location and try again.</h5>");
+              //   let resultsDiv = document.querySelector("#results");
+              $("#results").append("<h5>We discovered no results! Edit your location and try again.</h5>");
           }
-
+          
       }
     })
   })
@@ -155,10 +176,30 @@ function findARestaurant() {
 
 const renderProfile = `
   <h1>Profile</h1>
-`
-function displayUserProfile() {
-  document.querySelector("#profile").addEventListener("click", function(e) {
+  <h2>Welcome ${currentUserName}!</h2>
+  `
+  function displayUserProfile() {
+      document.querySelector("#profile").addEventListener("click", function(e) {
     // renderMainPage.style.display = "none";
     document.querySelector("#main-content").innerHTML = renderProfile;
-  })
-    }
+})
+};
+  
+//under construction - how to redirect to homepage with functionality?
+// function displayHomepage() {
+//     document.querySelector('#homepage').addEventListener('click', function(e) {
+//         loginPage.style.display = "none";
+//         document.querySelector("#main").innerHTML = renderMainPage;
+//     })
+// };
+        
+// const renderLastVisited = `
+// <h1>Last Visited</h1>
+// `;
+        
+// function displayLastVisited() {
+//     document.querySelector('#lastVisited').addEventListener('click', function(e) {
+//         document.querySelector("#main-content").innerHTML = renderLastVisited;
+//     })
+// };
+    
